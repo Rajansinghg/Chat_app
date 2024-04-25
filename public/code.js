2d521a890e3c8c2ffcfcc514cb3daf811f076f7f
aditya.mishra <aditya.mishra@mobzway.in>
2024-04-25 10:25:03 +0530

    added pursonal chat feature and active user feature

--- public/code.js
+++ public/code.js
@@ -1,9 +1,12 @@
 (function () {
     const app = document.querySelector(".app");
+    var audio = new Audio("ting.mp3");
     const socket = io();
+    let chatterUser = null;
     let uname;
+    let receiverUser = null;
     app.querySelector(".join-screen #join-user").addEventListener("click", function () {
-        let username = app.querySelector(".join-screen #username").value;
+        let username = (app.querySelector(".join-screen #username").value).charAt(0).toUpperCase() + (app.querySelector(".join-screen #username").value).slice(1);
         if (username.length == 0) {
             return;
         }
@@ -11,36 +14,62 @@
         uname = username;
         app.querySelector(".join-screen").classList.remove("active")
         app.querySelector(".chat-screen").classList.add("active")
+        // Added By Rajan 
+        const nameChatter = username;
+        socket.emit("new-user-joined", nameChatter);
+        // End
+
     });
     document.getElementById("username").addEventListener("keypress", function (event) {
         console.log("Rajan in enter on join screen--> ");
         if (event.key === "Enter") {
-        let username = app.querySelector(".join-screen #username").value;
-        if (username.length == 0) {
-            return;
+            let username = (app.querySelector(".join-screen #username").value).charAt(0).toUpperCase() + (app.querySelector(".join-screen #username").value).slice(1);
+            if (username.length == 0) {
+                return;
+            }
+            socket.emit("newuser", username);
+            uname = username;
+            app.querySelector(".join-screen").classList.remove("active")
+            app.querySelector(".chat-screen").classList.add("active")
+            // Added By Rajan 
+            const nameChatter = username;
+            socket.emit("new-user-joined", nameChatter);
+            // End
         }
-        socket.emit("newuser", username);
-        uname = username;
-        app.querySelector(".join-screen").classList.remove("active")
-        app.querySelector(".chat-screen").classList.add("active")}
+    });
+
+    app.querySelector(".chat-screen #groupchat").addEventListener("click", function () { // added for group chat btn
+        receiverUser = null;
+        document.getElementById("messages1").innerHTML = ""
+        document.getElementById("logo1").innerText = "GroupChat";
     });
 
     app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
+
+
         let message = app.querySelector(".chat-screen #message-input").value;
         console.log(message);
         if (message.length == 0) {
             return;
         }
-        console.log(uname, message);
+
         readerMessage("my", {
             username: uname,
             text: message
         });
 
-        socket.emit("chat", {
-            username: uname,
-            text: message
-        });
+        if (receiverUser != null) {
+
+            socket.emit("sendMsg", { msg: message, receiverUser: receiverUser });
+
+        } else {
+            console.log("receiverUser is Empty click--> ");
+            console.log(uname, message);
+            socket.emit("chat", {
+                username: uname,
+                text: message
+            });
+        }
 
         app.querySelector(".chat-screen #message-input").value = "";
     });
@@ -53,16 +82,24 @@
             if (message.length == 0) {
                 return;
             }
-            console.log(uname, message);
+
             readerMessage("my", {
                 username: uname,
                 text: message
             });
 
-            socket.emit("chat", {
-                username: uname,
-                text: message
-            });
+            if (receiverUser != null) {
+
+                socket.emit("sendMsg", { msg: message, receiverUser: receiverUser });
+
+            } else {
+                console.log("receiverUser is Empty click--> ");
+                console.log(uname, message);
+                socket.emit("chat", {
+                    username: uname,
+                    text: message
+                });
+            }
 
             app.querySelector(".chat-screen #message-input").value = "";
         }
@@ -78,10 +115,65 @@
         readerMessage("update", update);
     });
 
+    socket.on("chat1", function (data) {
+        console.log("Socket chat1 --> ", data)
+        readerMessage("other", data);
+    });
     socket.on("chat", function (message) {
         console.log("Socket chat --> ")
         readerMessage("other", message);
     });
+    // Added By rajan --> 
+    const OnlineUserContainer = document.querySelector(".OnlineUserContainer");
+    socket.on("connectedUserList", data => {
+        console.log("connectedUserList Called --> ");
+        document.getElementById("test2").innerHTML = ""
+        const ul = document.createElement('ul');
+        Object.keys(data).map(keyName => {
+            if (keyName !== socket.id) {
+                const li = document.createElement('li');
+                li.textContent = data[keyName];
+                li.setAttribute("id", keyName);
+                li.setAttribute("class", "user-name")
+                ul.appendChild(li);
+                OnlineUserContainer.append(ul);
+            }
+        })
+    })
+
+    OnlineUserContainer.addEventListener("click", function (event) {
+        console.log("list got response -->");
+        if (event.target.classList.contains("user-name")) {
+            const getId = event.target.getAttribute("id"); // Use event.target instead of event
+            console.log("chatterUser==== ", chatterUser)
+            if (chatterUser !== getId) {
+                chatterUser = getId
+                document.getElementById("messages1").innerHTML = ""
+                document.getElementById("logo1").innerText = event.target.textContent;
+            }
+            const playerChat = {
+                userId: getId,
+                name: event.target.textContent
+            }
+            receiverUser = getId
+        }
+    });
+
+
+    function sendmsg() {
+        const msg = document.getElementById("messageInp").value;
+        if (!receiverUser) {
+            alert("please select a connected User")
+        }
+        console.log("receiverUser ----------- ", receiverUser)
+        if (msg.length != 0 && receiverUser != null) {
+            socket.emit("sendMsg", { msg: msg, receiverUser: receiverUser });
+            append(`${nameChatter}: ${msg}`, "right")
+        }
+        document.getElementById("messageInp").value = ""
+    }
+
+    // End -->
 
     function readerMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
@@ -98,6 +190,7 @@
             messageContainer.appendChild(el);
         }
         else if (type == "other") {
+            audio.play();
             let el = document.createElement("div");
             console.log("El other--> ", el);
             el.setAttribute("class", "message other-message");
@@ -108,6 +201,7 @@
             </div>`;
             messageContainer.appendChild(el);
         } else if (type == "update") {
+            audio.play();
             let el = document.createElement("div");
             console.log("El update--> ", el);
             el.setAttribute("class", "update");
--- server.js
+++ server.js
@@ -6,9 +6,29 @@ const server =  require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
+const users = {};
+
 app.use(express.static(path.join(__dirname+"/public")));
 
 io.on("connection",function(socket){
+
+
+    socket.on("new-user-joined", name => {
+        users[socket.id] = name;
+        io.emit("connectedUserList", users);
+        console.log("users --> "+users)
+    });
+    socket.on("sendMsg", (data) => {
+        const socketId = data.receiverUser
+        // io.to(socketId).emit("recive", { message: data.msg, name: users[socket.id] })
+        io.to(socketId).emit("chat1", { text: data.msg, username: users[socket.id]})
+    });
+    socket.on("disconnect", message => {
+        delete users[socket.id];
+    });
+
+
+    // console.log("Socket id --> ",socket.rooms);
     socket.on("newuser",function(username){
         socket.broadcast.emit("update",username+" joined the conversation");
     });
